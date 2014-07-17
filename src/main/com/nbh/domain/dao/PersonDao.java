b5bb8ad545b55bb332855cd82f46896b11821985
Chaos <chaochao1104@gmail.com>
2014-07-17 22:38:08 +0800

    integrate with Transactionmanager

---
+++ src/main/com/nbh/domain/dao/PersonDao.java
@@ -0,0 +1,30 @@
+package com.nbh.domain.dao;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import com.nbh.domain.entity.demo.Person;
+
+public class PersonDao {
+	
+	private SessionFactory sessionFactory;
+
+	public SessionFactory getSessionFactory() {
+		return sessionFactory;
+	}
+
+	public void setSessionFactory(SessionFactory sessionFactory) {
+		this.sessionFactory = sessionFactory;
+	}
+	
+	public Session openSession() {
+		return sessionFactory.openSession();
+	}
+	
+	public List<Person> findAllPoeple() {
+		List<Person> people = openSession().createQuery("from person").list();
+		return people;
+	}
+}
---
+++ test/main/com/chaos/demo/TransactionManagerTest.java
@@ -0,0 +1,22 @@
+package com.chaos.demo;
+
+import java.util.List;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.nbh.domain.dao.PersonDao;
+import com.nbh.domain.entity.demo.Person;
+
+public class TransactionManagerTest {
+
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("app-context-nbh.xml");
+		PersonDao dao = (PersonDao) context.getBean("personDao");
+		List<Person> list = dao.findAllPoeple();
+		for (Person p : list) {
+			System.out.println(p.getWeight());  
+		}
+	}
+
+}
